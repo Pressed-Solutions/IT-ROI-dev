e5fd4d8a0ec98fd28d56bea1631e864d37eacb19
Andrew Minion <andrew@andrewrminion.com>
2014-08-07 22:26:43 -0400

    add the loop and upcoming events

--- single-event.php
+++ single-event.php
@@ -25,6 +25,10 @@ get_header(); ?>
 <div class="view view-latest-articles view-id-latest_articles view-display-id-block_1 view-dom-id-d2aad2774da5445702cc7a30b6ba6da6">
 
 <div class="view-content"  id="Top">
+   
+    <?php // the loop
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    
     <div class="col-md-8 main-tt maincon">
         <h2 class="page-header">Events</h2>
         <p>We are on a mission to make PPM simpler, and we're telling the world.<br>
@@ -62,6 +66,64 @@ get_header(); ?>
     </div>
     </div>
 
+    <div class="container dmbs-container">
+        <div class="col-md-8 main-tt">
+            <h3 class="UPCOMING-EVENTS">UPCOMING EVENTS</h3>
+
+            <?php $loop = new WP_Query( array( 'post_type' => 'event', 'posts_per_page' => 4, 'paged' => get_query_var( 'paged' ) ) ); ?>
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <div class="evt-post" id="post-<?php the_ID(); ?>">
+                <div class="col-md-3 evt-thumbnail">
+                    <?php if ( has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail('full'); ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-9 evt-content"><a href="<?php echo get_permalink(); ?>" ><div class="evt-title"><?php the_title(); ?></div></a>
+                    <div class="evt-date"><?php the_field('date'); ?> <?php the_field('time'); ?></div>
+                    <div class="register-button"><a href="#" >Register Now</a></div><?php the_content(); ?>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <?php endwhile; ?>
+
+
+
+
+
+            <!--<div class="pagen">
+            <div class="navigation">
+            <?php
+            // Bring $wp_query into the scope of the function
+            global $wp_query;
+
+            // Backup the original property value
+            $backup_page_total = $wp_query->max_num_pages;
+
+            // Copy the custom query property to the $wp_query object
+            $wp_query->max_num_pages = $loop->max_num_pages;
+            ?>
+            <?php if (function_exists("pagination")) {
+            pagination($custom_query->max_num_pages);
+            } ?>
+
+            <?php
+            // Finally restore the $wp_query property to it's original value
+            $wp_query->max_num_pages = $backup_page_total;
+            ?>
+            <div class="clear"></div>
+            </div>
+            </div>-->
+
+        </div>
+
+    <?php endwhile; ?>
+    <?php else: ?>
+
+    <?php get_404_template(); ?>
+
+    <?php endif; ?>
+
+    </div>
 </div><!-- div.view_content -->
 
 
