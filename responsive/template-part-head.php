8401ff5aba8f3e7bcb8dcf36402ea24fb62b248f
Andrew Minion <andrew@andrewrminion.com>
2014-08-20 15:52:17 -0400

    add secondary menu above main menu

--- responsive/template-part-head.php
+++ responsive/template-part-head.php
@@ -2,7 +2,7 @@
 
 <div class="top_menu">
 	<div class="container">
- 	<?php wp_nav_menu(array('theme_location' => "top_menu")); ?>
+    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu' => 'menu1', 'menu_class' => 'menu')); ?>  </div>
 	</div>
    </div>
 
