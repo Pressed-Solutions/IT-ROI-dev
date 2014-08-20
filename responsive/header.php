b720f35b79c644e99071f6bc2164e840253d4590
Andrew Minion <andrew@andrewrminion.com>
2014-08-20 16:26:26 -0400

    fix typo in header
    enqueue styles rather than scripts

--- responsive/header.php
+++ responsive/header.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php
-        add_action( 'wp_enqueue_scripts', 'add_bootstrap_styles' );
+        add_action( 'wp_enqueue_styles', 'add_bootstrap_styles' );
         function add_bootstrap_styles() {
-            wp_enqueue_script( 'responsive-style', get_template_directory_uri() . '/responsive/style.css' );
+            wp_enqueue_style( 'responsive-style', get_template_directory_uri() . '/responsive/style.css' );
         }
     ?>
     <?php wp_head(); ?>
