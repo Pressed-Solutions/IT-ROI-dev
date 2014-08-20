545160b0a3342f3c30dc7491675ba119f4160858
Andrew Minion <andrew@andrewrminion.com>
2014-08-20 14:34:21 -0400

    fix link to enqueued bootstrap CSS

--- responsive/header.php
+++ responsive/header.php
@@ -6,7 +6,7 @@
     <title><?php wp_title('&laquo;', true, 'right'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <?php wp_enqueue_script( 'responsive-style', get_template_directory_uri() . 'responsive/styles.css' ); ?>
+    <?php wp_enqueue_script( 'responsive-style', get_template_directory_uri() . '/responsive/styles.css' ); ?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
