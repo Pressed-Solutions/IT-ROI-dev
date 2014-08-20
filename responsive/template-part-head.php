6d33b9088146eb50f49e4cfbe4a33b9cce438db2
Andrew Minion <andrew@andrewrminion.com>
2014-08-20 16:45:57 -0400

    remove .dmbs-header-img (defaults to bootstrap logo)

--- responsive/template-part-head.php
+++ responsive/template-part-head.php
@@ -11,17 +11,3 @@
 <!--header-wrapper start-->
 
 	<div class="container dmbs-container">
-    
-        <div class="row dmbs-header">
-    
-            <?php if ( get_header_image() != '' || get_header_textcolor() != 'blank') : ?>
-    
-            <?php if ( get_header_image() != '' ) : ?>
-                <div class="col-md-4 dmbs-header-img text-center">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
-                </div>
-            <?php endif; ?>
-        
-            <?php endif; ?>
-            
-        </div>
