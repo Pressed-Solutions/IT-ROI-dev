554b70768a58217eaa40337132a217911d084150
Andrew Minion <andrew@andrewrminion.com>
2014-08-25 15:50:35 -0400

    copy responsive template to post-type template for use on live site with share point post type

---
+++ ppmbossassignment-responsive.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: PPM ideaBoss Responsive
+*/
+
+get_header( 'responsive' ); ?>
+
+<?php get_template_part('responsive/template-part', 'head'); ?>
+
+<?php get_template_part('responsive/template-part', 'topnav-content'); ?>
+
+<!-- start content container -->
+<div class="row dmbs-content">
+
+    <div class="col-md-12 dmbs-main">
+
+        <?php // theloop
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <h2 class="page-header"><?php the_title() ;?></h2>
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+
+        <?php endwhile; ?>
+        <?php else: ?>
+
+            <?php get_404_template(); ?>
+
+        <?php endif; ?>
+
+    </div>
+
+
+</div><!-- .row .dmbs-content -->
+<!-- end content container -->
+
+<?php get_footer( 'responsive' ); ?>
