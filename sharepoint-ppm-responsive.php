c2ba75815d74b828d99d94dc126dc7f740bfe23c
Andrew Minion <andrew@andrewrminion.com>
2014-09-11 20:12:47 -0400

    pull other PPM Boss suite products from database rather than needing them in the post content

--- sharepoint-ppm-responsive.php
+++ sharepoint-ppm-responsive.php
@@ -33,4 +33,34 @@ get_header( 'responsive' ); ?>
 </div><!-- .row .dmbs-content -->
 <!-- end content container -->
 
+<div class="row ppm-suite">
+    <h2 class="sharepoint">More solutions in the PPM Boss suite</h2>
+    <section class="ppm-links clearfix">
+        <?php
+        $id=$post->ID;
+        $args =  array(
+            'post_status' => 'publish',
+            'post_type' =>'sharepoint',
+            'post__not_in'   => array($id)
+        );
+
+        $ppm_loop = new WP_Query( $args );
+
+        while ( $loop->have_posts() ) : $loop->the_post();
+            $showfooter = get_field( "add_in_footer_module" );
+            if ( $showfooter == "yes" ) {
+                $this_product = get_field('this_products_name'); ?>
+
+                <section class="<?php echo strtolower($this_product) ?> col-md-6">
+                    <a href="<?php the_permalink(); ?>" class="rollover">
+                        <span class="hidden"><?php echo $this_product; ?></span>
+                    </a>
+                    <p><?php echo get_field( "footer_text" ); ?></p>
+                    <p><a href="<?php the_permalink(); ?>">Go to <?php echo $this_product ?> page</a></p>
+                </section><!-- .col-md-6 -->
+            <?php  }
+        endwhile; ?>
+    </section><!-- .ppm-links -->
+</div><!-- .ppm-suite -->
+
 <?php get_footer( 'responsive' ); ?>
