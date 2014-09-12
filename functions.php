f53930d462d3b491368dfd538462e2ece00a3194
Andrew Minion <andrew@andrewrminion.com>
2014-09-12 14:49:18 -0400

    add debugging info

--- functions.php
+++ functions.php
@@ -2025,5 +2025,5 @@ function pagination($pages = '', $range = 2)
 if ( strpos( get_page_template(), 'responsive' ) !== 0 ) {
     require_once('responsive/functions.php');
 }
-
+echo '<!-- debugging get_page_template: ' . get_page_template() . ' -->';
 ?>
