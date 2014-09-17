0c4f01e69e1b5513e5448414723f2bc62a3e2ce1
Andrew Minion <andrew@andrewrminion.com>
2014-09-17 09:09:02 -0400

    allow 3 menu levels

--- responsive/template-part-topnav-content.php
+++ responsive/template-part-topnav-content.php
@@ -18,7 +18,7 @@
 			<?php
             wp_nav_menu( array(
                     'theme_location'    => 'main_menu',
-                    'depth'             => 2,
+                    'depth'             => 3,
                     'container'         => 'div',
                     'container_class'   => 'collapse navbar-collapse navbar-1-collapse',
                     'menu_class'        => 'nav navbar-nav',
