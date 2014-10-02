9a35f26e019798a0f5e5fb280ee23a3323c5178f
Andrew Minion <andrew@andrewrminion.com>
2014-10-02 12:13:41 -0400

    update integration template with footer links to other integration products

--- responsive-integration.php
+++ responsive-integration.php
@@ -9,6 +9,7 @@ get_header( 'responsive' ); ?>
 
 <?php get_template_part('responsive/template-part', 'topnav-content'); ?>
 
+<div class="row integration-header clearfix"></div>
 <!-- start content container -->
 <div class="row dmbs-content">
 
@@ -33,34 +34,29 @@ get_header( 'responsive' ); ?>
 </div><!-- .row .dmbs-content -->
 <!-- end content container -->
 
-<div class="row ppm-suite">
-    <h2 class="sharepoint">More solutions in the PPM Boss suite</h2>
-    <section class="ppm-links clearfix">
-        <?php
-        $id=$post->ID;
-        $args =  array(
-            'post_status' => 'publish',
-            'post_type' =>'sharepoint',
-            'post__not_in'   => array($id)
-        );
+<div class="row integration-footer">
+    <h3>Integration tools:</h2>
+    <?php
+    $id=$post->ID;
+    $args =  array(
+        'post_status' => 'publish',
+        'post_type' =>'integration',
+        'post__not_in'   => array($id)
+    );
 
-        $ppm_loop = new WP_Query( $args );
+    $integration_loop = new WP_Query( $args );
 
-        while ( $ppm_loop->have_posts() ) : $ppm_loop->the_post();
-            $showfooter = get_field( "add_in_footer_module" );
-            if ( $showfooter == "yes" ) {
-                $this_product = get_field('this_products_name'); ?>
+    if ( $ppm_lookp->have_posts() ) {
+        echo '<ul class="integration-products">';
 
-                <section class="<?php echo strtolower($this_product) ?> col-md-6">
-                    <a href="<?php the_permalink(); ?>" class="rollover">
-                        <span class="hidden"><?php echo $this_product; ?></span>
-                    </a>
-                    <p><?php echo get_field( "footer_text" ); ?></p>
-                    <p><a href="<?php the_permalink(); ?>">Go to <?php echo $this_product ?> page</a></p>
-                </section><!-- .col-md-6 -->
-            <?php  }
-        endwhile; ?>
-    </section><!-- .ppm-links -->
-</div><!-- .ppm-suite -->
+        while ( $integration_loop->have_posts() ) : $integration_loop->the_post(); ?>
+            <li class="integration-product">
+                <a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+            </li><!-- .integration-product -->
+        <?php endwhile;
+
+        echo '</ul><!-- .integration-products -->';
+    } ?>
+</div><!-- .integration-footer -->
 
 <?php get_footer( 'responsive' ); ?>
