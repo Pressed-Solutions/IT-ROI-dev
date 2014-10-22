a796426b745d622c4902ceda7b5dfca2daa85949
Andrew Minion <andrew@andrewrminion.com>
2014-10-22 17:04:50 -0400

    bugfix update to events page

--- page-events.php
+++ page-events.php
@@ -36,6 +36,11 @@ $loop = new WP_Query( array(
         'meta_key'       => 'begin_date',
     ) );
 $counter = 1;
+
+if ( $loop->have_posts()) {
+    if ( ! is_paged() ) { echo '<h3 class="event-list-header">Next Event</h3>'; }
+}
+
 while ( $loop->have_posts() ) : $loop->the_post();
 
     $begin_date_raw = strtotime( get_field( 'begin_date' ) );
@@ -47,7 +52,8 @@ while ( $loop->have_posts() ) : $loop->the_post();
     }
 
     // format date as a nice range
-    $full_date = format_event_date($begin_date_raw, $end_date_raw);
+    $full_date = format_event_date( $begin_date_raw, $end_date_raw );
+    $datetime = date( 'c', $begin_date_raw );
 
     // get custom taxonomy name and image
     $category_array = get_the_terms( $loop->ID, 'event_type' );
@@ -56,8 +62,7 @@ while ( $loop->have_posts() ) : $loop->the_post();
         $category_thumb = z_taxonomy_image_url( $this_category->term_id );
     }
 
-    if ($counter == 1) { // first item
-?>
+    if ( ( $counter == 1 ) && ( ! is_paged() ) ) { // first item on first page ?>
     <div class="dmbs-container">
         <div class="event-blue-bg"></div>
         <div class="container dmbs-container main-event">
@@ -75,21 +80,28 @@ while ( $loop->have_posts() ) : $loop->the_post();
                         <?php the_title(); ?>
                     </h2>
                     <?php the_content(); ?>
-                    <div class="evt-date"><?php echo $full_date; ?> <div class="evt-time"><?php the_field('time'); ?></div></div>
+                    <time class="evt-date" datetime="<?php echo $datetime ?>"><?php echo $full_date; ?>
+                        <?php if ( get_field( 'time' ) ) { ?>
+                        <span class="evt-time"><?php the_field( 'time' ); ?></span>
+                        <?php } ?>
+                    </time>
+                    <?php if ( get_field( 'location' ) ) { ?>
+                        <div class="evt-location"><?php echo the_field( 'location'); ?></div>
+                    <?php } ?>
                     <div class="register-button"><?php echo get_post_meta( get_the_ID(), 'register_now', true ); ?></div><!-- .register-button -->
                 </div><!-- .col-sm-6 -->
             </div><!-- .col-md-12.main-tt.container -->
 
         </div><!-- .container.dmbs-container.main-event -->
     </div><!-- .dmbs-container -->
+    <?php } // end first item, non-paged
 
+    if ( ( ( $counter == 2 ) && ( ! is_paged() ) ) || ( ( $counter == 1 ) && ( is_paged() ) ) ) { // second item on page 1 or first item on paged ?>
     <div class="container dmbs-container">
         <div class="col-md-8 col-md-offset-2 main-tt events-list upcoming-events">
-            <h3>Upcoming Events</h3>
-    <?php
-        $counter = $counter + 1; // increment counter
-    } // end first item
-    else { // all other items
+            <h3 class="event-list-header">Upcoming Events</h3>
+    <?php } // end second item on page 1 or first item on paged
+    if ( ( $counter > 1 ) || ( is_paged() ) ) { // all other items or all items on pages after the first
         ?>
         <div class="evt-post" id="post-<?php the_ID(); ?>">
             <div class="col-md-3 evt-thumbnail">
@@ -97,14 +109,22 @@ while ( $loop->have_posts() ) : $loop->the_post();
             </div>
             <div class="col-md-9 evt-content clearfix">
                 <div class="evt-title"><?php the_title(); ?></div>
-                <div class="evt-date"><?php echo $full_date; ?> <?php the_field('time'); ?></div>
+                <time class="evt-date" datetime="<?php echo $datetime ?>"><?php echo $full_date; ?>
+                    <?php if ( get_field( 'time' ) ) { ?>
+                    <span class="evt-time"><?php the_field( 'time' ); ?></span>
+                    <?php } ?>
+                </time>
+                <?php if ( get_field( 'location' ) ) { ?>
+                    <div class="evt-location"><?php echo the_field( 'location'); ?></div>
+                <?php } ?>
                 <div class="register-button"><?php echo get_post_meta( get_the_ID(), 'register_now', true ); ?></div><!-- .register-button -->
                 <p><?php the_content(); ?></p>
             </div><!-- .col-md-9.evt-content -->
         </div><!-- .evt-post -->
     <?php
           $counter = $counter + 1; // increment counter
-    }; // end list of other items
+    }; // end list of other 4 items
+    $counter = $counter + 1; // increment counter
     endwhile; ?>
 
         <div class="pagen">
@@ -118,13 +138,11 @@ while ( $loop->have_posts() ) : $loop->the_post();
 
             // Copy the custom query property to the $wp_query object
             $wp_query->max_num_pages = $loop->max_num_pages;
-            ?>
-            <?php if (function_exists("pagination")) {
-            pagination($custom_query->max_num_pages);
-            } ?>
+            if ( function_exists("pagination") ) {
+                pagination( $custom_query->max_num_pages );
+            }
 
-            <?php
-            // Finally restore the $wp_query property to it's original value
+            // Finally restore the $wp_query property to its original value
             $wp_query->max_num_pages = $backup_page_total;
             ?>
             </div><!-- .navigation -->
@@ -134,11 +152,13 @@ while ( $loop->have_posts() ) : $loop->the_post();
 </div><!-- .container.dmbs-container -->
 
 <?php
-endwhile; // end $loop for upcoming events ?>
+endwhile; // end $loop for upcoming events
+
+if ( ! is_paged() ) { // show past events only on first page ?>
 
 <div class="container dmbs-container">
     <div class="col-md-8 col-md-offset-2 main-tt events-list past-events">
-        <h3>Past Events</h3>
+        <h3 class="event-list-header">Past Events</h3>
 
 <?php // rewind the loop to get past events
 rewind_posts();
@@ -169,7 +189,14 @@ while ( $loop->have_posts() ) : $loop->the_post();
             </div>
             <div class="col-md-9 evt-content clearfix">
                 <div class="evt-title"><?php the_title(); ?></div>
-                <div class="evt-date"><?php echo $full_date; ?> <?php the_field('time'); ?></div>
+                <time class="evt-date" datetime="<?php echo $datetime ?>"><?php echo $full_date; ?>
+                    <?php if ( get_field( 'time' ) ) { ?>
+                    <span class="evt-time"><?php the_field( 'time' ); ?></span>
+                    <?php } ?>
+                </time>
+                <?php if ( get_field( 'location' ) ) { ?>
+                    <div class="evt-location"><?php echo the_field( 'location'); ?></div>
+                <?php } ?>
                 <div class="register-button"><?php echo get_post_meta( get_the_ID(), 'register_now', true ); ?></div><!-- .register-button -->
                 <p><?php the_content(); ?></p>
             </div><!-- .col-md-9.evt-content -->
@@ -178,6 +205,7 @@ while ( $loop->have_posts() ) : $loop->the_post();
     </div><!-- .past-events -->
 
 <?php endwhile; // end $loop for past events ?>
+<?php } // end is_paged check ?>
 <?php else: // if check for original page content ?>
 
 <?php endif; // end original loop for page content ?>
