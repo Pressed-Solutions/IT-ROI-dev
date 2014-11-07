2895c2949b51f8655bb18c91e7fc6bcf3eb49891
Andrew Minion <andrew@andrewrminion.com>
2014-11-07 09:10:50 -0500

    add page template for reporting based on basic page

---
+++ page-reporting.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Pressed-Responsive Reporting
+*/
+
+get_header( 'responsive' ); ?>
+
+<?php get_template_part('responsive/template-part', 'head'); ?>
+
+<?php get_template_part('responsive/template-part', 'topnav-content'); ?>
+
+<!-- start content container -->
+<div class="row dmbs-content">
+
+    <div class="col-md-12 dmbs-main">
+
+        <?php // theloop
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+        <?php wp_link_pages(); ?>
+
+        <?php endwhile; ?>
+        <?php else: ?>
+
+            <?php get_404_template(); ?>
+
+        <?php endif; ?>
+
+    </div>
+
+
+</div><!-- .row .dmbs-content -->
+<!-- end content container -->
+
+<?php get_footer( 'responsive' ); ?>
